2742d12ed88917e5a739308aa394046093015320
David Dantas <davidalexandredantas@gmail.com>
2024-06-05 15:43:28 +0100

    fixed copies

--- projects/dex/assets/js/templates.js
+++ projects/dex/assets/js/templates.js
@@ -1,39 +1,61 @@
-export const nft_loan_template = `
-<div class="nft-card" id="nft-x">
-  <div style="display: flex; flex-direction=column;">
-    <div style="text-align: center;margin-right: 5px;display: flex;flex-direction: column; justify-content: space-between;">
-      <div class="new-badge" style="visibility:hidden;" id="nft-x-badge">New</div>
-    <img src="img_url" alt="img" class="copy-image" style="width: 50px; height: 50px;" id="nft-x-img">
-    <span id="nft-x-tooltip" class="tooltip" style="visibility: hidden;">Copied</span>
-      <div style="display: flex; margin-top: 5px; justify-content: center;" id="nft-x-percentage-paid">
-        <div style="width: 40px;"><svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <linearGradient id="gradloan_id" x1="0%" y1="100%" x2="0%" y2="0%">
-                <stop offset="_paid_offset" style="stop-color:rgb(76, 175, 80);stop-opacity:1" />
-                <stop offset="_paid_offset" style="stop-color:rgb(255, 255, 255,0);stop-opacity:1" />
-                <stop offset="_unpaid_offset" style="stop-color:rgb(255, 255, 255,0);stop-opacity:1" />
-              </linearGradient>
-            </defs>
-            <circle cx="50" cy="50" r="45" fill="url(#gradloan_id)" stroke="black" stroke-width="1" />
-          </svg>
+//loan card template
+export function loanTemplate(loan, payment, tokenUri) {
+  let deadline = new Date(loan.deadline * 1000).toLocaleString();
+  deadline = deadline.substring(0, deadline.length - 3);
+  let paid = parseInt(payment);
+  let amount = parseInt(loan.amount);
+  let percent = (paid / amount) * 100;
+  return `
+  <div class="loan-card" id="loan-${loan.id}">
+    <div style="display: flex; flex-direction=column;">
+      <div style="text-align: center;margin-right: 5px;display: flex;flex-direction: column; justify-content: space-between;">
+        <div class="new-badge" style="visibility:hidden;" id="loan-${loan.id}-badge">New</div>
+      `
+      + (tokenUri ? `<img src="${tokenUri}" alt="img" class="copy-image" style="width: 50px; height: 50px;" id="loan-${loan.id}-img" onClick="copyToClipboard('${tokenUri}')" >` : '') +
+      `
+      <span id="loan-${loan.id}-tooltip" class="tooltip" style="visibility: hidden;">Copied</span>
+      `
+      +((loan.lender != "0x0000000000000000000000000000000000000000")?
+      `
+        <div style="display: flex; margin-top: 5px; justify-content: center;" id="loan-${loan.id}-percentage-paid">
+          <div style="width: 40px;"><svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <linearGradient id="grad${loan.id}" x1="0%" y1="100%" x2="0%" y2="0%">
+                  <stop offset="${percent + "%"}" style="stop-color:rgb(76, 175, 80);stop-opacity:1" />
+                  <stop offset="${percent + "%"}" style="stop-color:rgb(255, 255, 255,0);stop-opacity:1" />
+                  <stop offset="${100 - percent + "%"}" style="stop-color:rgb(255, 255, 255,0);stop-opacity:1" />
+                </linearGradient>
+              </defs>
+              <circle cx="50" cy="50" r="45" fill="url(#grad${loan.id})" stroke="black" stroke-width="1" />
+            </svg>
+          </div>
+        </div>
+      `:'')
+      +
+      `</div>
+      <div style="display: flex; flex-direction: column; margin-right: 15px;">
+        <p id="loan-${loan.id}-loan-id" style="margin: 0 0;">loan#${loan.id}</p>
+        `
+        + (tokenUri ? `<p id="loan-${loan.id}-id" style="margin: 0 0;">nft#${loan.nftId}</p>` : '') + `
+        <!-- show borrower  and lender and deadline and ammount copy icon-->
+        <div class="loan-info">
+          <div class="borrower address"><span>Borrower: </span><span id="loan-${loan.id}-borrower">${loan.borrower.substring(0, 5) + ".."}</span></div>
+          `
+          +((loan.lender != "0x0000000000000000000000000000000000000000")?
+          `<div class="dress" id="loan-${loan.id}-lender"><span>Lender: </span><span id="loan-${loan.id}-lender">${loan.lender.substring(0, 5) + ".."}</span></div>`:'')
+          +
+          `
+          <div class="deadline">Deadline: <span id="loan-${loan.id}-deadline">${deadline}</span></div>
+          <div id="loan-${loan.id}-amount-container">Amount: <span id="loan-${loan.id}-amount">${loan.amount}</span> Wei</div>
+          <div id="loan-${loan.id}-paid-container">Paid: <span id="loan-${loan.id}-paid">${paid}</span> Wei</div>
         </div>
       </div>
+      `
+      + 
+      ((loan.lender == "0x0000000000000000000000000000000000000000" && loan.isBasedNft)?`<button style="float: inline-end;" onclick="loanByNft(${loan.nftId})" id="loan-${loan.id}-lend">Lend</button>`:``)
+      +
+      `
     </div>
-    
-
-    <div style="display: flex; flex-direction: column; margin-right: 15px;">
-      <p id="nft-x-loan-id" style="margin: 0 0;">loan#loan_id</p>
-      <p id="nft-x-id" style="margin: 0 0;">nft#nft_id</p>
-      <!-- show borrower  and lender and deadline and ammount copy icon-->
-      <div class="loan-info">
-        <div class="borrower address"><span>Borrower: </span><span id="nft-x-borrower">0x899..</span></div>
-        <div class="dress" id="nft-x-lender"><span>Lender: </span><span id="nft-x-lender">0x000..</span></div>
-        <div class="deadline">Deadline: <span id="nft-x-deadline">dead_lines</span></div>
-        <div id="nft-x-amount-container">Amount: <span id="nft-x-amount">due_amount</span> Wei</div>
-        <div id="nft-x-paid-container">Paid: <span id="nft-x-paid">paid_amount</span> Wei</div>
-      </div>
-    </div>
-    <button style="float: inline-end;" onclick="loanByNft(nft_id)" id="nft-x-lend">Lend</button>
   </div>
-</div>
-`;
+  `;
+}
--- projects/dex/assets/js/utils.js
+++ projects/dex/assets/js/utils.js
@@ -6,32 +6,24 @@ export const checkImage = (img) => {
       "https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/link.png";
   };
 };
-let copy_button_template = `
-<button class="copy-button"><svg  height="15px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" xmlns="http://www.w3.org/2000/svg"><path d="M5 15C4.06812 15 3.60218 15 3.23463 14.8478C2.74458 14.6448 2.35523 14.2554 2.15224 13.7654C2 13.3978 2 12.9319 2 12V5.2C2 4.0799 2 3.51984 2.21799 3.09202C2.40973 2.71569 2.71569 2.40973 3.09202 2.21799C3.51984 2 4.0799 2 5.2 2H12C12.9319 2 13.3978 2 13.7654 2.15224C14.2554 2.35523 14.6448 2.74458 14.8478 3.23463C15 3.60218 15 4.06812 15 5M12.2 22H18.8C19.9201 22 20.4802 22 20.908 21.782C21.2843 21.5903 21.5903 21.2843 21.782 20.908C22 20.4802 22 19.9201 22 18.8V12.2C22 11.0799 22 10.5198 21.782 10.092C21.5903 9.71569 21.2843 9.40973 20.908 9.21799C20.4802 9 19.9201 9 18.8 9H12.2C11.0799 9 10.5198 9 10.092 9.21799C9.71569 9.40973 9.40973 9.71569 9.21799 10.092C9 10.5198 9 11.0799 9 12.2V18.8C9 19.9201 9 20.4802 9.21799 20.908C9.40973 21.2843 9.71569 21.5903 10.092 21.782C10.5198 22 11.0799 22 12.2 22Z" stroke="currentColor" stroke-width="inherit" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>
-`;
+let copy_button_template = (text) => {
+  return `
+  <button class="copy-button" onClick="copyToClipboard('${text}')">
+  
+  <svg  height="15px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" xmlns="http://www.w3.org/2000/svg"><path d="M5 15C4.06812 15 3.60218 15 3.23463 14.8478C2.74458 14.6448 2.35523 14.2554 2.15224 13.7654C2 13.3978 2 12.9319 2 12V5.2C2 4.0799 2 3.51984 2.21799 3.09202C2.40973 2.71569 2.71569 2.40973 3.09202 2.21799C3.51984 2 4.0799 2 5.2 2H12C12.9319 2 13.3978 2 13.7654 2.15224C14.2554 2.35523 14.6448 2.74458 14.8478 3.23463C15 3.60218 15 4.06812 15 5M12.2 22H18.8C19.9201 22 20.4802 22 20.908 21.782C21.2843 21.5903 21.5903 21.2843 21.782 20.908C22 20.4802 22 19.9201 22 18.8V12.2C22 11.0799 22 10.5198 21.782 10.092C21.5903 9.71569 21.2843 9.40973 20.908 9.21799C20.4802 9 19.9201 9 18.8 9H12.2C11.0799 9 10.5198 9 10.092 9.21799C9.71569 9.40973 9.40973 9.71569 9.21799 10.092C9 10.5198 9 11.0799 9 12.2V18.8C9 19.9201 9 20.4802 9.21799 20.908C9.40973 21.2843 9.71569 21.5903 10.092 21.782C10.5198 22 11.0799 22 12.2 22Z" stroke="currentColor" stroke-width="inherit" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+  
+  </button>
+  `;
+}
 export function addButtonCopy(parent, text) {
-  parent.innerHTML += copy_button_template;
-  let button = parent.lastElementChild;
-  addCopyListener(button, text);
+  parent.innerHTML += copy_button_template(text);
 }
 
-export function addCopyListener(element, text) {
-  console.log("Adding copy listener");
-  element.addEventListener("click", function () {
-    navigator.clipboard
-      .writeText(text)
-      .then(function () {
-        console.log("Copied to clipboard");
-        //show tooltip
-        let tooltip = element.parentElement.querySelector(".tooltip");
-        tooltip.style.visibility = "visible";
-        setTimeout(() => {
-          tooltip.style.visibility = "hidden";
-        }, 1000);
-      })
-      .catch(function () {
-        console.log("Failed to copy to clipboard");
-      });
+export function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(function () {
+    console.log("Copied to clipboard");
+  }, function (err) {
+    console.error("Failed to copy to clipboard", err);
   });
 }
 
